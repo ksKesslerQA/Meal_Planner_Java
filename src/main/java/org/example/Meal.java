3c152539e2c1f0d2e668e291bc264e5058aba4a0
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2026-01-14 18:03:49 +0000

    Stage 2 complete

--- src/main/java/org/example/Meal.java
+++ src/main/java/org/example/Meal.java
@@ -27,26 +27,69 @@ public class Meal {
         return ingredients;
     }
 
-    static void addNewMeal(){
-            Scanner scan = new Scanner(System.in);
+    static Meal addNewMeal(Scanner scan){
 
+        String mealCategory;
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
-        String mealCategory = scan.nextLine();
 
+        while (true) {
+            mealCategory = scan.nextLine();
+
+            if (mealCategory.equals("breakfast") ||
+                    mealCategory.equals("lunch") ||
+                    mealCategory.equals("dinner")) {
+                break; // вихід з циклу, якщо все ок
+            } else {
+                System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+            }
+        }
+
+        String nameOfMeal;
         System.out.println("Input the meal's name:");
-        String nameOfMeal = scan.nextLine();
 
+        while (true) {
+            nameOfMeal = scan.nextLine();
+
+            if (nameOfMeal.matches("[a-zA-Z ]+")) {
+                break;
+            } else {
+                System.out.println("Wrong format. Use letters only!");
+            }
+        }
+
+        List<String> ingredients;
         System.out.println("Input the ingredients:");
-        String ingredientsInput = scan.nextLine();
 
-        List<String> ingredients = Arrays.stream(ingredientsInput.split(","))
-                .map(String::trim)
-                .toList();
+        while (true) {
+            String ingredientsInput = scan.nextLine();
+
+            List<String> tempIngredients = Arrays.stream(ingredientsInput.split(","))
+                    .map(String::trim)
+                    .toList();
+
+            boolean allValid = true;
+
+            for (String ingredient : tempIngredients) {
+                if (!ingredient.matches("[a-zA-Z ]+")) {
+                    allValid = false;
+                    break;
+                }
+            }
+
+            if (allValid) {
+                ingredients = tempIngredients;
+                break;
+            } else {
+                System.out.println("Wrong format. Use letters only!");
+            }
+        }
 
         Meal addedMeal = new Meal(mealCategory, nameOfMeal, ingredients);
 
-        printMealInfo(addedMeal);
+        //printMealInfo(addedMeal);
         System.out.println("The meal has been added!");
+
+        return addedMeal;
 
     };
 
