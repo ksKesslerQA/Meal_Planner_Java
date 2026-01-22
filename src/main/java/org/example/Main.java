7ea946a791f119c921a99793ac1989a75e5fd570
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2026-01-22 18:14:04 +0000

    updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.app.ConsoleApp;
 import org.example.dao.*;
 import org.example.model.DaysOfTheWeek;
 import org.example.model.Meal;
@@ -20,96 +21,28 @@ public class Main {
         String PASS = "root";
 
         Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-        MealDao mealDao = new MealDao(connection);
-        PlanDao planDao = new PlanDao(connection);
-        PlanService planService = new PlanService(mealDao, planDao);
-        ShoppingListService shoppingListService = new ShoppingListService(planDao);
-        connection.setAutoCommit(true);
 
         DatabaseInitializer.createTables(connection);
 
+        MealDao mealDao = new MealDao(connection);
+        PlanDao planDao = new PlanDao(connection);
+
+        PlanService planService = new PlanService(mealDao, planDao);
+        ShoppingListService shoppingListService = new ShoppingListService(planDao);
+
         Scanner scan = new Scanner(System.in);
 
-        String operation = "";
-        while(!operation.equals("exit")) {
-            System.out.println("What would you like to do (add, show, plan, list plan, save, reset, exit)?");
-            operation = scan.nextLine();
+        ConsoleApp app = new ConsoleApp(
+                connection,
+                mealDao,
+                planDao,
+                planService,
+                shoppingListService,
+                scan
+        );
 
-            if (operation.equals("add")) {
-                Meal meal = Meal.addNewMeal(scan);
+        app.run();
 
-                int mealId = mealDao.getNextMealId();
-                mealDao.saveMeal(meal, mealId);
-                mealDao.saveIngredients(meal.getIngredients(), mealId);
-            } else if (operation.equals("show")) {
-                // List<Meal> mealsFromDb = mealDao.getAllMeals();
-                String mealCategory;
-                System.out.println("Which category do you want to print (breakfast, lunch, dinner)?");
-                while (true) {
-                    mealCategory = scan.nextLine();
-
-                    if (mealCategory.equals("breakfast") ||
-                            mealCategory.equals("lunch") ||
-                            mealCategory.equals("dinner")) {
-                        break;
-                    } else {
-                        System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
-                    }
-                }
-                List<Meal> mealsFromDbByCategory = mealDao.getAllMealsByCategory(mealCategory);
-
-                if (mealsFromDbByCategory.isEmpty()) {
-                    System.out.println("No meals found.");
-                } else {
-                    System.out.println("Category: " + mealCategory);
-
-                    for (Meal meal : mealsFromDbByCategory) {
-                        Meal.printMealInfo(meal);
-                    }
-                    System.out.println();
-                }
-            } else if (operation.equals("plan")) {
-                planService.createWeeklyPlan(scan);
-
-            } else if (operation.equals("list plan")) {
-                List<MealPlan> plans = planDao.getWeeklyPlan();
-
-                if (plans.isEmpty()) {
-                    System.out.println("Database does not contain any meal plans");
-                } else {
-                    for (MealPlan plan : plans) {
-                        System.out.println(plan.getDay());
-                        System.out.println("Breakfast: " + plan.getBreakfast());
-                        System.out.println("Lunch: " + plan.getLunch());
-                        System.out.println("Dinner: " + plan.getDinner());
-                        System.out.println();
-                    }
-                }
-
-            } else if (operation.equals("save")) {
-                if (planDao.getWeeklyPlan().isEmpty()) {
-                    System.out.println("Unable to save. Plan your meals first.");
-                } else {
-                    System.out.println("Input a filename:");
-                    String filename = scan.nextLine();
-
-                    shoppingListService.saveShoppingList(filename);
-
-                    System.out.println("Saved!");
-                }
-
-            } else if (operation.equals("reset")) {
-
-                DatabaseCleaner.clearAll(connection);
-                DatabaseSeeder.seed(mealDao);
-                System.out.println("Database reset.");
-
-            } else if (operation.equals("exit")) {
-                System.out.println("Bye!");
-            } else {
-                continue;
-            }
-        }
 
     }
 }
